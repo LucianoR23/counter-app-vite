b15ddb71c66a85f894fe56e8f507191528ccf5cf
LucianoR23 <231097lr@gmail.com>
2023-03-06 12:47:38 -0300

    Terminado Seccion 4 Primeros pasos en React

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -5,14 +5,31 @@
 // export default App;
 // import {Fragment} from 'react';
 
-const newMessage = "Subtitulo"
-export const FirstApp = () => {
+import PropTypes from 'prop-types';
 
+const newMessage = "Subtitulo"
+export const FirstApp = ({title}) => {
+  // if (!title) {
+  //   throw new Error('El title no existe');
+  // }
+  // console.log(props);
 
   return (
     <>
-        <h1>First App</h1>
+        <h1>{title}</h1>
         <h2>{newMessage}</h2>
     </>
   )
+}
+
+
+// Prop y default prop al final
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  subTitle : 'No hay subtitulo',
+  name: "Luciano"
 }
