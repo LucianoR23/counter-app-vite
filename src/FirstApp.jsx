6d476e02225cbb76241aa26f3777a527a65c3190
LucianoR23 <231097lr@gmail.com>
2023-03-06 08:55:59 -0300

    Para la notebook, recordar dar yarn i

---
+++ src/FirstApp.jsx
@@ -0,0 +1,18 @@
+// function App() {
+//     return <h1>Hola Mundo</h1>;
+// };
+
+// export default App;
+// import {Fragment} from 'react';
+
+const newMessage = "Subtitulo"
+export const FirstApp = () => {
+
+
+  return (
+    <>
+        <h1>First App</h1>
+        <h2>{newMessage}</h2>
+    </>
+  )
+}
